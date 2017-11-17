9e691cb21394a00a0deaa09e057b9b2673b65932
Nail Khasipov <nail.khasipov@gmail.com>
2017-11-17 23:37:05 +0500

    reset and lap functionality

--- main.js
+++ main.js
@@ -1,60 +1,105 @@
-var Stopwatch = function() {
-  this.interval = null;
+var Stopwatch = function(config) {
+  this.running = false;
+  this.time = null;
   this.timestamp = null;
-
-  this.drawActions();
+  this.timer = config.timer;
+  this.stopButton = config.stopButton;
+  this.startButton = config.startButton;
+  this.resetButton = config.resetButton;
+  this.lapButton = config.lapButton;
+  this.results = config.results;
 }
 
 Stopwatch.prototype.start = function() {
-  this.timestamp = Date.now();
-  this.interval = setInterval(function(){
-    this.print();
-  }.bind(this), 1);
-  this.drawActions();
+  if (!this.timestamp)
+    this.timestamp = Date.now();
+  this.running = true;
+  this.step();
+  this.showStop();
+  this.showLap();
 }
 
 Stopwatch.prototype.stop = function() {
-  clearInterval(this.interval);
-  this.interval = null;
-  this.drawActions();
+  this.running = false;
+  this.showStart();
+  this.showReset();
+}
+
+Stopwatch.prototype.reset = function() {
+  if (!this.timestamp) {
+    return;
+  } else {
+    this.timestamp = null;
+    this.time = null;
+  }
+  this.running = false;
+  this.timer.innerText = '00:00:00';
+  this.results.innerHTML = '';
+  this.showStart();
+  this.showLap(true);
+}
+
+Stopwatch.prototype.lap = function() {
+  if (!this.time)
+    return;
+  let li = document.createElement('li');
+  li.innerText = this.format();
+  this.results.appendChild(li);
+}
+
+Stopwatch.prototype.step = function() {
+  if (!this.running) 
+    return;
+  this.time = new Date(Date.now() - this.timestamp);
+  this.print();
+  requestAnimationFrame(this.step.bind(this));
 }
 
 Stopwatch.prototype.print = function() {
-  let current = new Date(Date.now() - this.timestamp);
-  
-  let minutes = zeroPad(current.getMinutes(), 2);
-  let seconds = zeroPad(current.getSeconds(), 2);
-  let milliseconds = zeroPad(Math.floor(current.getMilliseconds()/10, 2), 2);
-
-  document.getElementById('timer').innerText = `${minutes}:${seconds}:${milliseconds}`;
+  timer.innerText = this.format();
 }
 
-Stopwatch.prototype.drawActions = function() {
-  let startButton = document.createElement("button");
-  startButton.innerText = 'Start';
-  startButton.onclick = this.start.bind(this);
+Stopwatch.prototype.format = function() {
+  return zeroPad(this.time.getMinutes(), 2) + ':' +
+         zeroPad(this.time.getSeconds(), 2) + ':' +
+         zeroPad(Math.floor(this.time.getMilliseconds()/10, 2), 2);
+}
 
-  let stopButton = document.createElement("button");
-  stopButton.innerText = 'Stop';
-  stopButton.onclick = this.stop.bind(this);
+Stopwatch.prototype.showStart = function() {
+  this.stopButton.classList.add('hidden');
+  this.startButton.classList.remove('hidden');
+}
 
-  let resetButton = document.createElement("button");
-  resetButton.className = 'disable';
-  resetButton.innerText = 'Reset';
-  resetButton.onclick = this.start.bind(this);
+Stopwatch.prototype.showStop = function() {
+  this.stopButton.classList.remove('hidden');
+  this.startButton.classList.add('hidden');
+}
 
-  document.getElementById("actions").innerHTML = "";
-
-  if (this.interval) {
-    document.getElementById("actions").appendChild(resetButton);
-    document.getElementById("actions").appendChild(stopButton);
+Stopwatch.prototype.showLap = function(disable) {
+  if (disable) {
+    this.lapButton.classList.add('disable');
   } else {
-    document.getElementById("actions").appendChild(resetButton);
-    document.getElementById("actions").appendChild(startButton);
+    this.lapButton.classList.remove('disable');
   }
+  this.resetButton.classList.add('hidden');
+  this.lapButton.classList.remove('hidden');
 }
 
-let stopwatch = new Stopwatch();
+Stopwatch.prototype.showReset = function() {
+  this.resetButton.classList.remove('hidden');
+  this.lapButton.classList.add('hidden');
+}
+
+const config = {
+  timer: document.getElementById('timer'),
+  stopButton: document.getElementById('stopButton'),
+  startButton: document.getElementById('startButton'),
+  resetButton: document.getElementById('resetButton'),
+  lapButton: document.getElementById('lapButton'),
+  results: document.getElementById('results')
+}
+
+let stopwatch = new Stopwatch(config);
 
 function zeroPad(number, width) {
   var string = String(number);
